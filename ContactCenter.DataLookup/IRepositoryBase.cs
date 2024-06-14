5ad78d0d882e6ee1ab4978bf4c9382409bbeb2ed
Shaik, Aziz <Aziz.Shaik@dish.com>
2024-06-14 00:47:47 -0600

    clean up and updates.

--- ContactCenter.DataLookup/IRepositoryBase.cs
+++ ContactCenter.DataLookup/IRepositoryBase.cs
@@ -1,6 +1,13 @@
-﻿namespace ContactCenter.DataLookup
+﻿using Amazon.DynamoDBv2.DocumentModel;
+
+namespace ContactCenter.DataLookup
 {
     public interface IRepositoryBase<T> where T : class
     {
+        public Task<T> CreateAsync(Document document);
+        public Task<T?> ReadAsync(string primaryKey, string sortKey = null);
+        public Task<T?> UpdateAsync(Document document);
+        public Task DeleteAsync(string primaryKey, string sortKey = null);
+        
     }
 }
--- ContactCenter.IntentLookup.cs/Function.cs
+++ ContactCenter.IntentLookup.cs/Function.cs
@@ -1,4 +1,6 @@
+using Amazon.Lambda.ConnectEvents;
 using Amazon.Lambda.Core;
+using Microsoft.Extensions.DependencyInjection;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -7,15 +9,43 @@ namespace ContactCenter.IntentLookup.cs;
 
 public class Function
 {
-    
+
+    private readonly IntentRepo intentRepo;
+    private readonly string _defaultQueue;
+    public Function()
+    {
+        Console.WriteLine("In constructor, starting to build dependencies.");
+        var startup = new Startup();
+        IServiceProvider provider = startup.ConfigureServices();
+        intentRepo = provider.GetRequiredService<IntentRepo>();
+        _defaultQueue = Environment.GetEnvironmentVariable("DefaultIntent");
+        Console.WriteLine("Done building dependencies.");
+
+    }
+
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
     /// <param name="input">The event for the Lambda function handler to process.</param>
     /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public async Task<IntentResponse> FunctionHandler(ContactFlowEvent input, ILambdaContext context)
     {
-        return input.ToUpper();
+        context.Logger.Log("Fetching the queue info from the db");
+        if (input.Details.Parameters.TryGetValue("intent", out string intent))
+        {
+            var queue = await intentRepo.ReadAsync(intent);
+            if (queue != null)
+            {
+                return new IntentResponse { QueueArn = queue.QueueArn };
+            }
+        }
+        return new IntentResponse { QueueArn = _defaultQueue };
+
     }
+}
+
+public class IntentResponse
+{
+    public string QueueArn { get; set; }
 }
